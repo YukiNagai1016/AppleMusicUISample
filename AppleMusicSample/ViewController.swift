da71e3936da6e241ededd505a843edb0e58aaa4a
優樹永井 <wadachi0505@gmail.com>
2020-01-17 18:44:55 +0900

    Initial Commit

---
+++ AppleMusicSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMusicSample
+//
+//  Created by 優樹永井 on 2020/01/17.
+//  Copyright © 2020 com.nagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
